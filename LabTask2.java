a9ff54753e2cad8e3733adb77f40e521d35294a9
Moazam986 <118812964+Moazam986@users.noreply.github.com>
2022-11-26 08:46:16 +0500

    Add files via upload

---
+++ LabTask2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class LabTask2
+{
+		public static void main(String args[])
+		{
+		Scanner ob=new Scanner(System.in);	
+
+				System.out.println("\nEnter any number");
+				int even=ob.nextInt();
+	
+	if(even%2==0)
+		
+				System.out.println("\nEVEN NUMBER");
+			
+	else
+	
+				System.out.println("\nODD NUMBER");
+	}
+}
